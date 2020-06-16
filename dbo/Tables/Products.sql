80fda385a1cccb357b5843875204b57bc8a6b7db
Vsevolod Zakharchenko <vzakha@softserveinc.com>
2020-06-16 19:42:29 +0300

    init

---
+++ dbo/Tables/Products.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [ProductID]   INT             NOT NULL,
+    [Name]        NVARCHAR (100)  NOT NULL,
+    [Description] NVARCHAR (500)  NOT NULL,
+    [Category]    NVARCHAR (50)   NOT NULL,
+    [Price]       DECIMAL (16, 2) NOT NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ProductID] ASC)
+);
+
